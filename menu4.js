aeecff6ecbc331634d476544869a4def498163a4
Alvaro0rtegaPadilla <159319407+Alvaro0rtegaPadilla@users.noreply.github.com>
2024-05-07 14:06:17 -0500

    p1

--- menu4.js
+++ menu4.js
@@ -9,16 +9,28 @@ fetch('menu.json')
         menuContainer.appendChild(categoryTitle);
 
         const table = document.createElement('table');
-        const tableHead = `<tr><th>Foto</th><th>Descripcion</th<th>Precio</th<th>Seleccionar</th></tr>`;
-        let tableBody = '';
+        const tableContent = `<tr><th>Foto</th><th>Descripcion</th<th>Precio</th<th>Seleccionar</th></tr>`;
+        
 
         category.items.forEach( item => {
-            tableBody +=`<tr>
+            tableContent += `<tr>
             <td><img src="${item.image}" alt="${item.name}"></td>
             <td>${item.name} - ${item.description}</td>
             <td>${item.price}</td>
-            <td><input type="checkbox" data-price="${item.price.replacee('&','')}"onchange="updateTotal()"</td>
-            </tr>`
-        })
-    })
-})
+            <td><input type="checkbox" data-price="${item.price.replace('$','')}"onchange="updateTotal()"</td>
+            </tr>`;
+        });
+        table.innerHTML = tableContent;
+        menuContainer.appendChild(table);
+    });
+});
+    function updateTotal(){
+        const checkboxes = document.querySelectorAll('input[type = "checkbox"] [data-price]');
+        let currentTotal = 0;
+        checkboxes.forEach(checkboxes =>{
+            if(checkbox.checked){
+                currentTotal += parseFloat(checkbox.getAttribute('data-price'));
+            }
+        });
+        document.getElementById('total').text = currentTotal.toFixed(2);
+    }
